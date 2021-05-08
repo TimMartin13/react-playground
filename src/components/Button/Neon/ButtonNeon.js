6c56a1d4abc93f5cc7b5f680154f5458c88ae32a
Tim Martin <timmartin13@gmail.com>
2021-05-08 14:25:49 -0500

    Added neon button

---
+++ src/components/Button/Neon/ButtonNeon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ButtonNeon.scss';
+
+const ButtonNeon = () => {
+  return (
+    <div className="neon-background">
+      <a href="#" className="neon-button">Nyx</a>
+    </div>
+  )
+}
+
+export default ButtonNeon;
